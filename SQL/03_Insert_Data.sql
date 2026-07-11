cf04a08a79c0b3ad882f249ba0d3c9054d2b368f
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-11 17:41:52 +0530

    Create 03_Insert_Data.sql

---
+++ SQL/03_Insert_Data.sql
@@ -0,0 +1,21 @@
+/*
+============================================
+Import CSV Data
+============================================
+
+1. Open MySQL Workbench.
+
+2. Right-click Sales table.
+
+3. Select Table Data Import Wizard.
+
+4. Choose:
+
+Sales_Performance_Dataset_200_Rows.csv
+
+5. Finish Import.
+
+This project uses the CSV file available in:
+
+Dataset/
+*/
